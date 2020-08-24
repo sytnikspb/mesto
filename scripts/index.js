821f81df70ea971ffabba76eb5d75c27a9aa8dc4
Alex Sytnik <sytnik.spb@yandex.ru>
2020-08-25 01:59:12 +0300

    ПР4 Место v2.0 Реализован функционал добавления-удаления карточки с фотографией, лайки, попап-превью фотографии, плавное открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,124 @@
-const popup = document.querySelector('.popup');
-const popupContainer = popup.querySelector('.popup__container');
+const defaultCards = [
+  {
+    name: 'Бали. Индонезия',
+    link: 'https://images.unsplash.com/photo-1482348838597-b97cc6c86a88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'
+  },
+  {
+    name: 'Сахара. Африка',
+    link: 'https://images.unsplash.com/photo-1489493585363-d69421e0edd3?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+  },
+  {
+    name: 'Мачу-Пикчу. Перу',
+    link: 'https://images.unsplash.com/photo-1415804941191-bc0c3bbac10d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+  },
+  {
+    name: 'Фьядрарглуфюр. Исландия',
+    link: 'https://images.unsplash.com/photo-1536610350990-079f9db69b55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80'
+  },
+  {
+    name: 'Этрета. Франция',
+    link: 'https://images.unsplash.com/photo-1541187714594-731deadcd16a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'
+  },
+  {
+    name: 'Ао Нанг. Тайланд',
+    link: 'https://images.unsplash.com/photo-1483683804023-6ccdb62f86ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80'
+  }
+];
+
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close-button');
 const profileMyName = document.querySelector('.profile__myname');
 const profileProfession = document.querySelector('.profile__profession');
-const inputMyName = popup.querySelector('.popup__input_type_myname');
-const inputProffesion = popup.querySelector('.popup__input_type_profession');
+const inputMyName = document.querySelector('.popup__input_type_myname');
+const inputProfession = document.querySelector('.popup__input_type_profession');
+const formEditProfile = document.querySelector('.popup__container_type_edit-profile');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 
-function popupToggle(event) {
-  if (!popup.classList.contains('popup_opened')) {
-    inputMyName.value = profileMyName.textContent;
-    inputProffesion.value = profileProfession.textContent;
-  }
-  if (event.currentTarget === event.target) {
-    popup.classList.toggle('popup_opened');
-  }
+const profileAddButton = document.querySelector('.profile__add-button');
+const inputPlaceName = document.querySelector('.popup__input_type_place-name');
+const inputPlaceLink = document.querySelector('.popup__input_type_place-link');
+const formAddPlace = document.querySelector('.popup__container_type_add-place');
+const popupAddPlace = document.querySelector('.popup_type_add-place');
+
+const places = document.querySelector('.places');
+const placeTemplate = document.querySelector('#place-template').content;
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
+
+const popupIllustration = document.querySelector('.popup_type_illustration');
+const illustrationImage = popupIllustration.querySelector('.popup__image');
+const illustrationName = popupIllustration.querySelector('.popup__caption');
+
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function formSubmitHandler(event) {
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function openEditProfile(popup) {
+  inputMyName.value = profileMyName.textContent;
+  inputProfession.value = profileProfession.textContent;
+
+  openPopup(popup);
+}
+
+function updateProfile(event) {
   event.preventDefault();
+
   profileMyName.textContent = inputMyName.value;
-  profileProfession.textContent = inputProffesion.value;
-  popupToggle(event);
+  profileProfession.textContent = inputProfession.value;
+
+  closePopup(event.target.closest('.popup'));
 }
 
-profileEditButton.addEventListener('click', popupToggle);
-popupContainer.addEventListener('submit', formSubmitHandler);
-popupCloseButton.addEventListener('click', popupToggle);
-popup.addEventListener('click', popupToggle);
-// popup.addEventListener добавляет закрытие не только по close-button, но и по свободной области вне popupContainer
+function removeElement(event) {
+  event.target.closest('.place').remove();
+}
+
+function toggleLike(event) {
+  event.currentTarget.classList.toggle('place__like-button_active');
+}
+
+function openIllustration(imgLink, caption) {
+  illustrationImage.src = imgLink;
+  illustrationImage.alt = caption;
+  illustrationName.textContent = caption;
+
+  openPopup(popupIllustration);
+}
+
+function createPlaceCard(imgLink, name) {
+  const placeElement = placeTemplate.cloneNode(true);
+  const placeImage = placeElement.querySelector('.place__image');
+
+  placeImage.src = imgLink;
+  placeElement.querySelector('.place__image').alt = name;
+  placeElement.querySelector('.place__title').textContent = name;
+
+  placeImage.addEventListener('click', () => openIllustration(imgLink, name));
+  placeElement.querySelector('.place__remove-button').addEventListener('click', removeElement);
+  placeElement.querySelector('.place__like-button').addEventListener('click', toggleLike);
+
+  return placeElement;
+}
+
+function addNewPlace(event) {
+  event.preventDefault();
+
+  const card = createPlaceCard(inputPlaceLink.value, inputPlaceName.value);
+
+  places.prepend(card);
+
+  closePopup(event.target.closest('.popup'));
+
+  formAddPlace.reset();
+}
+
+profileEditButton.addEventListener('click', () => openEditProfile(popupEditProfile));
+profileAddButton.addEventListener('click', () => openPopup(popupAddPlace));
+formEditProfile.addEventListener('submit', updateProfile);
+formAddPlace.addEventListener('submit', addNewPlace);
+popupCloseButtons.forEach(el => el.addEventListener('click', () => closePopup(event.target.closest('.popup'))));
+
+defaultCards.forEach(el => places.prepend(createPlaceCard(el.link, el.name)));
